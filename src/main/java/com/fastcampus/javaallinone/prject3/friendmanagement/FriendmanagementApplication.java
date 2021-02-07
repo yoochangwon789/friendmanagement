c1735f6b84e414bfffae32444967b58b1aa9585f
changwon <ckddnjs554@naver.com>
2021-02-07 20:27:16 +0900

    Feat : 지인 정보 관리 시스템

---
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/FriendmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaallinone.prject3.friendmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/FriendmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaallinone.prject3.friendmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
