d44cf94330c767f2b5751ea1845c6ae35d8b76e4
changwon <ckddnjs554@naver.com>
2021-02-08 22:29:20 +0900

    Feat : Person service blockNames 를 뺀 나머지 person 객체 출력 코드

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonService.java
@@ -1,8 +1,29 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.service;
 
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Block;
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Person;
+import com.fastcampus.javaallinone.prject3.friendmanagement.repository.BlockRepository;
+import com.fastcampus.javaallinone.prject3.friendmanagement.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
 
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private BlockRepository blockRepository;
+
+    public List<Person> getPeopleExcludeBlocks() {
+        List<Person> people = personRepository.findAll();
+        List<Block> blocks = blockRepository.findAll();
+        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
+
+        return people.stream().filter(person -> !blockNames.contains(person.getName())).collect(Collectors.toList());
+    }
 }
---
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonServiceTest.java
@@ -0,0 +1,7 @@
+package com.fastcampus.javaallinone.prject3.friendmanagement.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+}
