d0fd13d3043d500e4219ae2fcbd5efa265557eb7
changwon <ckddnjs554@naver.com>
2021-02-08 22:31:14 +0900

    TDD : getPeopleExcludeBlocks 구현 코드 작성을 위한 여러가지 객체 생성

--- src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonServiceTest.java
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonServiceTest.java
@@ -1,7 +1,27 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.service;
 
+import com.fastcampus.javaallinone.prject3.friendmanagement.repository.BlockRepository;
+import com.fastcampus.javaallinone.prject3.friendmanagement.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PersonServiceTest {
 
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private BlockRepository blockRepository;
+
+    @Test
+    void getPeopleExcludeBlocks() {
+
+    }
 }
