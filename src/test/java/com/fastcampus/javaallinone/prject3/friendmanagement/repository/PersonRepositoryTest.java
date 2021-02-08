acf99be75723674992356aff874ecae2e94afdf6
changwon <ckddnjs554@naver.com>
2021-02-08 20:20:38 +0900

    TDD : PersonRepository findAll 타입 주석 제거

--- src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersonRepositoryTest {
@@ -26,7 +24,7 @@ class PersonRepositoryTest {
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
 
         List<Person> people = personRepository.findAll();
 
