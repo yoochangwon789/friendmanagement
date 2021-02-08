0b94674a62c214af88ddf3a2045edf5b4ac0d4c3
changwon <ckddnjs554@naver.com>
2021-02-08 21:29:51 +0900

    TDD : Crud 테스트 코드 구현

---
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/BlockRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.fastcampus.javaallinone.prject3.friendmanagement.repository;
+
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Block;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockRepositoryTest {
+
+    @Autowired
+    private BlockRepository blockRepository;
+
+    @Test
+    void crud() {
+        Block block = new Block();
+        block.setName("martin");
+        block.setReason("친하지 않음");
+        block.setStartDate(LocalDate.now());
+        block.setEndDate(LocalDate.now());
+
+        blockRepository.save(block);
+
+        List<Block> blocks = blockRepository.findAll();
+
+        assertThat(blocks.size()).isEqualTo(1);
+        assertThat(blocks.get(0).getName()).isEqualTo("martin");
+    }
+}
