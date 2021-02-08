a849e18725855e5ba746c46824d0b6eb1c78dd59
changwon <ckddnjs554@naver.com>
2021-02-08 21:02:11 +0900

    TDD : Map 를 이용한 객체 비교

--- src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -44,5 +46,11 @@ class PersonRepositoryTest {
         System.out.println(person1.equals(person2));
         System.out.println(person1.hashCode());
         System.out.println(person2.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
     }
 }
