f93812d6f566cbaa00b153e110d66bdb315e3e6c
changwon <ckddnjs554@naver.com>
2021-02-08 20:17:03 +0900

    TDD : Person bloodType 테스트 코드 추가 작업

--- src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
@@ -22,6 +22,7 @@ class PersonRepositoryTest {
         Person person = new Person();
         person.setName("martin");
         person.setAge(10);
+        person.setBloodType("A");
 
         personRepository.save(person);
 
@@ -32,5 +33,6 @@ class PersonRepositoryTest {
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("martin");
         assertThat(people.get(0).getAge()).isEqualTo(10);
+        assertThat(people.get(0).getBloodType()).isEqualTo("A");
     }
 }
