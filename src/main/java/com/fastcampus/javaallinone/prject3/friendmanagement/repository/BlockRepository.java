369b4eba11d6d638932a2fd28013691248877cae
changwon <ckddnjs554@naver.com>
2021-02-08 21:22:48 +0900

    Feat : BlockRepository 추가

---
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/BlockRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.javaallinone.prject3.friendmanagement.repository;
+
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Block;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockRepository extends JpaRepository<Block, Long> {
+}
