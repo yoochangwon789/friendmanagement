06eb56470daa6155e0db9e13ae2b3b1e7117574d
changwon <ckddnjs554@naver.com>
2021-02-10 14:58:00 +0900

    Feat : PersonRepository findByName Query Method 추가

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepository.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepository.java
@@ -4,6 +4,10 @@ import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByName(String name);
 }
