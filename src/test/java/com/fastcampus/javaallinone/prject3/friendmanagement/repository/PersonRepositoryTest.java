57501f7c1649d56a1e21863fcccd0c0008efe6b0
changwon <ckddnjs554@naver.com>
2021-02-10 15:39:12 +0900

    TDD : findByBirthdayBetween 테스트 코드 구현 메서드 오버라이딩 사용

--- src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,7 +67,28 @@ class PersonRepositoryTest {
         result.forEach(System.out::println);
     }
 
+    @Test
+    void findByBirthdayBetween() {
+        givenPerson("martin", 10, "A", LocalDate.of(1991, 8, 15));
+        givenPerson("david", 9, "B", LocalDate.of(1992, 7, 10));
+        givenPerson("dennis", 8, "O", LocalDate.of(1993, 1, 5));
+        givenPerson("sophia", 7, "AB", LocalDate.of(1994, 6, 30));
+        givenPerson("benny", 6, "A", LocalDate.of(1995, 8, 30));
+
+        List<Person> result = personRepository.findByBirthdayBetween(LocalDate.of(1991, 8, 1),
+                LocalDate.of(1991, 8, 31));
+
+        result.forEach(System.out::println);
+    }
+
     private void givenPerson(String name, int age, String bloodType) {
-        personRepository.save(new Person(name, age, bloodType));
+        givenPerson(name, age, bloodType, null);
+    }
+
+    private void givenPerson(String name, int age, String bloodType, LocalDate birthday) {
+        Person person = new Person(name, age, bloodType);
+        person.setBirthday(birthday);
+
+        personRepository.save(person);
     }
 }
