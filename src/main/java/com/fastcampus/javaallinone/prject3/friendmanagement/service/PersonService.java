df54ada379a689b47a162d9d010746430a9608ab
changwon <ckddnjs554@naver.com>
2021-02-16 18:18:20 +0900

    Feat : PersonService getPerson 메서드를 에러 처리를 위해 로직 다시 구현

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/service/PersonService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,7 +28,7 @@ public class PersonService {
 
     @Transactional(readOnly = true)
     public Person getPerson(Long id) {
-        Person person = personRepository.findById(id).get();
+        Person person = personRepository.findById(id).orElse(null);
 
         log.info("person : {}", person);
 
