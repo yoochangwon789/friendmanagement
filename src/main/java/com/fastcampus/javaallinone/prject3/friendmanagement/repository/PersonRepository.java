d44c6382dc42f12ffc7e7e9768fb8afe11ebac84
changwon <ckddnjs554@naver.com>
2021-02-16 21:36:14 +0900

    Feat : PersonRepository findByPeopleDeleted Query 타입 메서드 구현

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepository.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepository.java
@@ -21,4 +21,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     // ? 인자를 사용하면 파라미터의 순서대로 쿼리를 넣어주게 된다
     @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")
     List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday);
+
+    @Query(value = "select person from Person person where person.deleted = true")
+    List<Person> findPeopleDeleted();
 }
