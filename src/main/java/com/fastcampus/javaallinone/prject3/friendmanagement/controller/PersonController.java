e90adf667c651bb3710edc97db101ca7851a0432
changwon <ckddnjs554@naver.com>
2021-02-16 18:14:46 +0900

    Feat : PersonController GetMapping 메서드 생성

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/PersonController.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/PersonController.java
@@ -1,8 +1,22 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.controller;
 
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Person;
+import com.fastcampus.javaallinone.prject3.friendmanagement.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value = "/api/person")
 @RestController
 public class PersonController {
 
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public Person getPerson(Long id) {
+        return personService.getPerson(id);
+    }
 }
