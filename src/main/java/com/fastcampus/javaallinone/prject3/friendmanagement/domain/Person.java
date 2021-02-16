b85c3a07d74bfd976d06e46df8bd2fc5859bb71f
changwon <ckddnjs554@naver.com>
2021-02-16 21:07:33 +0900

    Feat : PersonDto 의 StringUtils 를 이용한 setter 구현

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/domain/Person.java
@@ -3,6 +3,7 @@ package com.fastcampus.javaallinone.prject3.friendmanagement.domain;
 import com.fastcampus.javaallinone.prject3.friendmanagement.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.prject3.friendmanagement.domain.dto.Birthday;
 import lombok.*;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
@@ -53,6 +54,26 @@ public class Person {
     public void set(PersonDto personDto) {
         if (personDto.getAge() != 0) {
             this.setAge(personDto.getAge());
+        }
+
+        if (!StringUtils.isEmpty(personDto.getHobby())) {
+            this.setHobby(personDto.getHobby());
+        }
+
+        if (!StringUtils.isEmpty(personDto.getBloodType())) {
+            this.setBloodType(personDto.getBloodType());
+        }
+
+        if (!StringUtils.isEmpty(personDto.getAddress())) {
+            this.setAddress(personDto.getAddress());
+        }
+
+        if (!StringUtils.isEmpty(personDto.getJob())) {
+            this.setJob(personDto.getJob());
+        }
+
+        if (!StringUtils.isEmpty(personDto.getPhoneNumber())) {
+            this.setPhoneNumber(personDto.getPhoneNumber());
         }
     }
 }
