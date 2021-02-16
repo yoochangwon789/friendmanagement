cf101246d763042169c4a31416c627457672b039
changwon <ckddnjs554@naver.com>
2021-02-16 16:44:16 +0900

    Feat : data.sql 타입 추가 선업

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,5 @@
-insert into person(`id`, `name`, `age`, `blood_type`)values (1, 'martin', 10, 'A');
-insert into person(`id`, `name`, `age`, `blood_type`)values (2, 'david', 9, 'B');
-insert into person(`id`, `name`, `age`, `blood_type`)values (3, 'dennis', 8, 'O');
-insert into person(`id`, `name`, `age`, `blood_type`)values (4, 'sophia', 7, 'AB');
-insert into person(`id`, `name`, `age`, `blood_type`)values (5, 'benny', 6, 'A');
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`)values (1, 'martin', 10, 'A', 1991, 8, 15);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`)values (1, 'david', 9, 'A', 1992, 7, 21);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`)values (1, 'dennis', 8, 'A', 1993, 10, 15);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`)values (1, 'sophia', 7, 'A', 1994, 8, 31);
+insert into person(`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`)values (1, 'benny', 6, 'A', 1995, 12, 23);
