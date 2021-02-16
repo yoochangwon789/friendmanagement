414dd8ba99a751693cee1fa5c1f0506bdf45f41b
changwon <ckddnjs554@naver.com>
2021-02-16 18:10:30 +0900

    Feat : PersonController 생성

---
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/PersonController.java
@@ -0,0 +1,8 @@
+package com.fastcampus.javaallinone.prject3.friendmanagement.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+}
