ef572a917864b24623137c6ed096883126d20322
changwon <ckddnjs554@naver.com>
2021-02-16 16:37:43 +0900

    Feat : data.sql insert 쿼리문 4개 추가 작업

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,5 @@
 insert into person(`id`, `name`, `age`, `blood_type`)values (1, 'martin', 10, 'A');
+insert into person(`id`, `name`, `age`, `blood_type`)values (2, 'david', 9, 'B');
+insert into person(`id`, `name`, `age`, `blood_type`)values (3, 'dennis', 8, 'O');
+insert into person(`id`, `name`, `age`, `blood_type`)values (4, 'sophia', 7, 'AB');
+insert into person(`id`, `name`, `age`, `blood_type`)values (5, 'benny', 6, 'A');
