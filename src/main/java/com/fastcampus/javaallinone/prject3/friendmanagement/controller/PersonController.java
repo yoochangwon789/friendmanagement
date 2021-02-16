6dac89d6a9da6f9700d78416b54c88a1b2629190
changwon <ckddnjs554@naver.com>
2021-02-16 18:22:57 +0900

    Feat : PersonController @PathVariable 사용

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/PersonController.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/PersonController.java
@@ -3,10 +3,7 @@ package com.fastcampus.javaallinone.prject3.friendmanagement.controller;
 import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Person;
 import com.fastcampus.javaallinone.prject3.friendmanagement.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/api/person")
 @RestController
@@ -16,7 +13,8 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping
-    public Person getPerson(Long id) {
+    @RequestMapping(value = "/{id}")
+    public Person getPerson(@PathVariable Long id) {
         return personService.getPerson(id);
     }
 }
