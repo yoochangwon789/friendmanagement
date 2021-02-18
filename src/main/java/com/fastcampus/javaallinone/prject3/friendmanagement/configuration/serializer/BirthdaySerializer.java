f3790292093d43bb17c68ebe097d1c9f3817bc2d
changwon <ckddnjs554@naver.com>
2021-02-19 02:29:37 +0900

    Feat : BirthdaySerializer 로직 구현

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/configuration/serializer/BirthdaySerializer.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/configuration/serializer/BirthdaySerializer.java
@@ -6,11 +6,15 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class BirthdaySerializer extends JsonSerializer<Birthday> {
 
     @Override
     public void serialize(Birthday value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-
+        if (value != null) {
+            // 1991-08-15 << 값의 형태로 return 하기 위해 하는 작업
+            gen.writeObject(LocalDate.of(value.getYearOfBirthday(), value.getMonthOfBirthday(), value.getDayOfBirthday()));
+        }
     }
 }
