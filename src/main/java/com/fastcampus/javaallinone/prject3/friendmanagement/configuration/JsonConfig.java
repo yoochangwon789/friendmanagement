06d42fb42536677934e6a4806e84fde47c9183f3
changwon <ckddnjs554@naver.com>
2021-02-19 02:31:22 +0900

    Feat : JsonConfig MappingJackson2CborHttpMessageConverter Bean 추가

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/configuration/JsonConfig.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/configuration/JsonConfig.java
@@ -1,4 +1,20 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+
+@Configuration
 public class JsonConfig {
+
+    @Bean
+    public MappingJackson2CborHttpMessageConverter mappingJackson2CborHttpMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2CborHttpMessageConverter converter = new MappingJackson2CborHttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+
+        return converter;
+    }
+
+    @Bean
 }
