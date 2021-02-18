f07d17cbc5502c82f6a639d5afcb5c87c1009eb3
changwon <ckddnjs554@naver.com>
2021-02-19 02:35:32 +0900

    Feat : JsonConfig BirthdayModule 로직 구현

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/configuration/JsonConfig.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/configuration/JsonConfig.java
@@ -1,6 +1,9 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.configuration;
 
+import com.fastcampus.javaallinone.prject3.friendmanagement.configuration.serializer.BirthdaySerializer;
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.dto.Birthday;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
@@ -25,5 +28,14 @@ public class JsonConfig {
         objectMapper.registerModule();
 
         return objectMapper;
+    }
+    
+    // module 을 Serializer 를 맵핑 하는 작업
+    static class BirthdayModule extends SimpleModule {
+        BirthdayModule() {
+            super();
+            // 나는 Birthday 를 가져와 Birthday 를 Serializer 을 하겠다 라는 의미
+            addSerializer(Birthday.class, new BirthdaySerializer());
+        }
     }
 }
