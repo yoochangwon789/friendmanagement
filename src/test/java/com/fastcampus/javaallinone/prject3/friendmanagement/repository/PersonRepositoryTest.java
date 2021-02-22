57fadef5ba34859b781f6a2a14f0b29239fe8339
changwon <ckddnjs554@naver.com>
2021-02-22 20:40:32 +0900

    TDD : findByName PersonRepositoryTest 테스트 코드 구현

--- src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/prject3/friendmanagement/repository/PersonRepositoryTest.java
@@ -1,14 +1,17 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.repository;
 
 import com.fastcampus.javaallinone.prject3.friendmanagement.domain.Person;
+import com.fastcampus.javaallinone.prject3.friendmanagement.domain.dto.Birthday;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Transactional
 @SpringBootTest
@@ -17,5 +20,20 @@ class PersonRepositoryTest {
     @Autowired
     private PersonRepository personRepository;
 
+    @Test
+    void findByName() {
+        List<Person> people = personRepository.findByName("tony");
+        assertThat(people.size()).isEqualTo(1);
 
+        Person person = people.get(0);
+        assertAll(
+                () -> assertThat(person.getName()).isEqualTo("tony"),
+                () -> assertThat(person.getHobby()).isEqualTo("reading"),
+                () -> assertThat(person.getAddress()).isEqualTo("Seoul"),
+                () -> assertThat(person.getBirthday()).isEqualTo(Birthday.of(LocalDate.of(1991, 7, 10))),
+                () -> assertThat(person.getJob()).isEqualTo("officer"),
+                () -> assertThat(person.getPhoneNumber()).isEqualTo("010-2222-5555"),
+                () -> assertThat(person.isDeleted()).isEqualTo(false)
+        );
+    }
 }
