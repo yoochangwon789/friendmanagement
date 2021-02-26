7772d69934168cffa3f06421de2381664a55b58a
changwon <ckddnjs554@naver.com>
2021-02-26 18:36:45 +0900

    Feat : ErrorResponse of 메서드 추가 코드 리팩토링

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/exception/dto/ErrorResponse.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/exception/dto/ErrorResponse.java
@@ -1,13 +1,19 @@
 package com.fastcampus.javaallinone.prject3.friendmanagement.exception.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
-@AllArgsConstructor(staticName = "of")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorResponse {
 
     private int code;
 
     private String message;
+
+    public static ErrorResponse of(HttpStatus httpStatus, String message) {
+        return new ErrorResponse(httpStatus.value(), message);
+    }
 }
