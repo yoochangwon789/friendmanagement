2de2257ba0054e76b72c1817358b2f87bb02d010
changwon <ckddnjs554@naver.com>
2021-03-02 18:49:19 +0900

    Feat : PersonDto name 값 NotNull 추가

--- src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/dto/PersonDto.java
+++ src/main/java/com/fastcampus/javaallinone/prject3/friendmanagement/controller/dto/PersonDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Data
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class PersonDto {
 
+    @NotEmpty
     private String name;
 
     private String hobby;
